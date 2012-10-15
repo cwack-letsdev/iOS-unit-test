ae4f2b56175f97feda3180ccc107eef26472e332
Christian Wack <christian.wack@letsdev.de>
2012-10-15 15:09:34 +0200

    added first test

---
+++ iOS-Unit-Test-Example/GHunit/Tests/FirstTest.m
@@ -0,0 +1,29 @@
+//
+//  FirstTest.m
+//  iOS-Unit-Test-Example
+//
+//  Created by Christian Wack (cwack) on 15.10.12.
+//  Copyright (c) 2012 let's dev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <GHUnitIOS/GHUnit.h>
+
+@interface FirstTest : GHTestCase { }
+@end
+
+@implementation FirstTest
+
+- (void)testStrings {
+    NSString *string1 = @"a string";
+    GHTestLog(@"I can log to the GHUnit test console: %@", string1);
+    
+    // Assert string1 is not NULL, with no custom error description
+    GHAssertNotNULL(string1, nil);
+    
+        // Assert equal objects, add custom error description
+    NSString *string2 = @"a string";
+    GHAssertEqualObjects(string1, string2, @"A custom error message. string1 should be equal to: %@.", string2);
+}
+
+@end
