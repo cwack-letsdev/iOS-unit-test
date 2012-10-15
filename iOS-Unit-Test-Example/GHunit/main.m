5ae8599a83db0b8861bffb1da5f0fb6aad6ed15b
Christian Wack <christian.wack@letsdev.de>
2012-10-15 15:04:43 +0200

    added ghunit

---
+++ iOS-Unit-Test-Example/GHunit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GHunit
+//
+//  Created by Christian Wack (cwack) on 15.10.12.
+//  Copyright (c) 2012 let's dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, @"GHUnitIOSAppDelegate");
+    }
+}
